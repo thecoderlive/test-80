ae191f5c4f2c8263b3706428c322539508cfee71
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-02 13:34:38 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "b40adeef-0d2c-49ea-a615-9c0fd26b2301",
+    "home_feed": [
+        {
+            "id": "d97bed8f-2f3b-4d66-85d6-4e2880181faa",
+            "artist_name": "want",
+            "home_carousel": [
+                {
+                    "id": "413d8937-1a8f-4258-843d-57d63727a126",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/134.png"
+                },
+                {
+                    "id": "f342badc-2872-40a6-97d0-53336d1fe2b3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/333.png"
+                },
+                {
+                    "id": "a207f95a-7ae4-4a38-a790-5a1e9f3db63c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/110.png"
+                },
+                {
+                    "id": "bea1e8b0-5883-46e3-9c6e-e05ea2746705",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "5bf0af47-fdc0-47d9-abce-8af8fbb78c6f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "Fly myself decide science camera impact dog. Especially lawyer new if buy season language score. If only assume practice wall."
+        },
+        {
+            "id": "24994c29-b0d1-4beb-974b-cd87495d243b",
+            "artist_name": "how",
+            "home_carousel": [
+                {
+                    "id": "9ff81fe4-5538-41ae-be0c-d5101bd4c06c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/257.png"
+                },
+                {
+                    "id": "52adcdb4-6af5-44d3-84a6-52d028d67285",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/19.png"
+                },
+                {
+                    "id": "c2de2fc5-c3c5-4cb7-83c8-ad6d885a2379",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/295.png"
+                },
+                {
+                    "id": "b36b0cf3-1b9f-440e-a7f0-ff5e3cc08c2e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/293.png"
+                },
+                {
+                    "id": "584f772c-6726-496b-b35e-d9ee6e2a2a41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/101.png"
+                }
+            ],
+            "art_caption": "the",
+            "art_description": "Including eight face budget house manage. Environment soldier election TV task herself."
+        },
+        {
+            "id": "4c7b56ae-a587-4ead-b298-cd2f59478876",
+            "artist_name": "either",
+            "home_carousel": [
+                {
+                    "id": "affb15b0-4d0f-4273-93b7-8a339215814e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "2ca1f5e6-ceaa-4e5c-be82-3aa88ffa38c5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/121.png"
+                },
+                {
+                    "id": "27ebc668-bcc1-4821-95fa-833d4a3ab515",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/31.png"
+                },
+                {
+                    "id": "9ae21edf-72ea-4203-b9f7-1c52a4f03e04",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                },
+                {
+                    "id": "f5e73fd2-cee3-4133-a5da-56f4c5f04425",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/35.png"
+                }
+            ],
+            "art_caption": "entire",
+            "art_description": "Describe make official force consumer actually. Rise appear upon senior."
+        },
+        {
+            "id": "8eebf652-04a1-4618-b2ae-a2287621faec",
+            "artist_name": "study",
+            "home_carousel": [
+                {
+                    "id": "a9aae187-67c8-43b4-b01b-06ece73b3d04",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/227.png"
+                },
+                {
+                    "id": "c8cff516-0d61-4656-857e-671e9fab7f35",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/44.png"
+                },
+                {
+                    "id": "6a144f8f-4dcc-4bfe-b1f5-065b57e63d8a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/216.png"
+                },
+                {
+                    "id": "08dd6899-4ba0-437d-ad1a-cb84b6e7c96f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/224.png"
+                },
+                {
+                    "id": "7544d91a-cd3d-4b73-aabb-1d802aa0c71d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/284.png"
+                }
+            ],
+            "art_caption": "season",
+            "art_description": "Exist case particularly theory key probably improve keep. Sort sense head including single."
+        },
+        {
+            "id": "537c1a91-6fce-4bca-a474-6074c7e4adcd",
+            "artist_name": "me",
+            "home_carousel": [
+                {
+                    "id": "a1e860ec-b5de-49de-a1b8-3fb36e1306f9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/331.png"
+                },
+                {
+                    "id": "78a8b736-e260-4cc6-bf23-81894cce732b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/1.png"
+                },
+                {
+                    "id": "c4b8b428-7c4d-497d-8110-79df902d968f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "45761d09-5217-476c-af53-941f6533ea50",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/238.png"
+                },
+                {
+                    "id": "7bf30fb7-1d0b-4d0f-9037-19a87df36d40",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                }
+            ],
+            "art_caption": "answer",
+            "art_description": "Voice cut process similar high once. Probably put result political win oil head."
+        }
+    ]
+}
